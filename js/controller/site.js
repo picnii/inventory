da8def6e799120a0c40304fed37881733b2116ea
picnii <sompop@picnii.me>
2014-10-02 04:06:01 +0700

    update code

--- js/controller/site.js
+++ js/controller/site.js
@@ -92,6 +92,11 @@ function OrderCtrl($scope, $location)
 
 function PaymentCtrl($scope, $location)
 {
-	
+	$scope.orders = [
+		{number:1, name:"PomadeA", count:1, price:700},
+		{number:2, name:"PomadeB", count:1, price:1400},
+		{number:3, name:"PomadeC", count:1, price:750},
+		{number:4, name:"PomadeD", count:1, price:200}
+	];
 }
 
